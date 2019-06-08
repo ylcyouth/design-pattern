e9b1e991dee22d5613e9853010aafccaaf6e3039
ylcyouth <yx_163_1@163.com>
2019-06-08 21:38:46 +0800

    double check + volatile关键字实现兼顾性能和线程安全的单例模式

---
+++ src/main/java/com/ylcyouth/desigin/pattern/creational/p5_singleton/p4_doublechecksingleton/TestMultiThread.java
@@ -0,0 +1,21 @@
+package com.ylcyouth.desigin.pattern.creational.p5_singleton.p4_doublechecksingleton;
+
+/**
+ * @author ylcyouth
+ * @create 2019/6/8 19:15
+ */
+public class TestMultiThread {
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(new T());
+        Thread t2 = new Thread(new T());
+
+        t1.start();
+        t2.start();
+
+        System.out.println("program end...");
+
+
+    }
+}
