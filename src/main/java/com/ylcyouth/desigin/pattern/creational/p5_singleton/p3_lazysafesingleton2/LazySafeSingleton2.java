04975b541410b76236e4e76f6354b9827cb36449
ylcyouth <yx_163_1@163.com>
2019-06-08 20:22:53 +0800

    锁错了，修改

--- src/main/java/com/ylcyouth/desigin/pattern/creational/p5_singleton/p3_lazysafesingleton2/LazySafeSingleton2.java
+++ src/main/java/com/ylcyouth/desigin/pattern/creational/p5_singleton/p3_lazysafesingleton2/LazySafeSingleton2.java
@@ -1,7 +1,5 @@
 package com.ylcyouth.desigin.pattern.creational.p5_singleton.p3_lazysafesingleton2;
 
-import java.awt.*;
-
 /**
  * @author ylcyouth
  * @create 2019/6/8 20:13
@@ -12,7 +10,7 @@ public class LazySafeSingleton2 {
     private LazySafeSingleton2() {}
 
     public static LazySafeSingleton2 getInstance() {
-        synchronized(LayoutManager2.class) {
+        synchronized(LazySafeSingleton2.class) {
             if (instance == null) {
                 instance = new LazySafeSingleton2();
             }
